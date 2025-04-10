7838002a499725bb32c3fc07387eb109bf0b740f
CarmeloMelito <melitocarmelo211@gmail.com>
2025-04-10 09:55:18 +0200

    feat:create array post

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import ArticleList from "./Article";
+
+const post = [
+  {
+    id: 1,
+    title: "titolo",
+    description: "descrizione",
+  },
+  {
+    id: 2,
+    title: "titolo",
+    description: "descrizione",
+  },
+  {
+    id: 3,
+    title: "titolo",
+    description: "descrizione",
+  },
+  {
+    id: 4,
+    title: "titolo",
+    description: "descrizione",
+  },
+];
 
 function App() {
-  return <></>;
+  console.log(post);
+  return (
+    <>
+      <h1>Lista Articoli</h1>
+    </>
+  );
 }
 
 export default App;
