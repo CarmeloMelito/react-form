858523151e83d3b3f522be6d30989a929607ecf6
CarmeloMelito <melitocarmelo211@gmail.com>
2025-04-10 10:06:38 +0200

    feat:map post

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
 import React, { useState } from "react";
-import ArticleList from "./Article";
 
 const post = [
   {
     id: 1,
-    title: "titolo",
+    title: "titolo 1",
     description: "descrizione",
   },
   {
     id: 2,
-    title: "titolo",
+    title: "titolo 2",
     description: "descrizione",
   },
   {
     id: 3,
-    title: "titolo",
-    description: "descrizione",
-  },
-  {
-    id: 4,
-    title: "titolo",
+    title: "titolo 3",
     description: "descrizione",
   },
 ];
 
 function App() {
   console.log(post);
+
   return (
     <>
       <h1>Lista Articoli</h1>
+
+      {/* MAP */}
+      {post.map((post) => (
+        <article key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.description}</p>
+        </article>
+      ))}
     </>
   );
 }
